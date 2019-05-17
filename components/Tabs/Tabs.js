8e35120a00dc1b669df20d918708e37271a2244a
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2019-05-17 12:16:53 -0400

    My tabData was being grabbed from the document rather than the tabElement which caused the nulls. After fixing that, everything started working as intended. Was ablet o complete the sprint without futher issues. MVP requirements met.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -6,54 +6,60 @@ class TabLink {
     
     // Get the `data-tab` value from this.tabElement and store it here
     // this.tabData = ; 
-    this.tabData = document.querySelector(`.tab[data-tab="${this.tabElement.dataset.tab}"]`)
-    console.log(this.tabData)
+    this.tabData = this.tabElement.dataset.tab
+    
     // We need to find out if a user clicked 'all' cards or a specific category.  Follow the instructions below to accomplish this task:    
     
 
     // Check to see if this.tabData is equal to 'all'
-    if(this.tabData === 'ALL'){
+    if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
       // this.cards = ;
-        this.cards = Array.from(document.getElementsByClassName("card"))
+      this.cards = document.querySelectorAll(".card")
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
       // this.cards = ;
-        this.cards = document.querySelector(`.tab[data-tab="${this.tabData}"]`)
+        this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`)
       
     }
 
-      this.test = Array.from(document.getElementsByClassName("card"))
-      console.log(this.test)
-
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
     // this.cards = Array.from(this.cards).map();
-    console.log(this.cards)
-    this.cards = Array.from(this.cards).map(cards => {new TabCard(cards)})
+    this.cards = Array.from(this.cards).map(cards => new TabCard(cards))
 
     // Add a click event that invokes this.selectTab
     // this.tabElement.addEventListener();
+    this.tabElement.addEventListener('click', () => this.selectTab())
   }
 
   selectTab(){
 
     // Select all elements with the .tab class on them
     // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll('.tab')
     
     // Iterate through the NodeList removing the .active-tab class from each element
     // tabs.forEach()
+      tabs.forEach((el)=>{
+        el.classList.remove("active-tab")
+      })
 
     // Select all of the elements with the .card class on them
     // const cards = ;
+    const cards = document.querySelectorAll('.card')
 
     // Iterate through the NodeList setting the display style each one to 'none'
     // cards.forEach()
-    
+    cards.forEach((el) =>{
+      el.style.display = 'none'
+    })
     // Add a class of ".active-tab" to this.tabElement
     // this.tabElement;
+    this.tabElement.classList.add('active-tab')
   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
     // this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard())
   }
 }
 
@@ -61,10 +67,12 @@ class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
     // this.cardElement;
+    this.cardElement = cardElement;
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
     // this.cardElement;
+    this.cardElement.style.display = "flex"
   }
 
   }
